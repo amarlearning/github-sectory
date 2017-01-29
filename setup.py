d18a36e8c935eeabc806d6ae08055b4eff511bdf
Amar Prakash Pandey <amarlearning@users.noreply.github.com>
2017-01-29 16:47:24 +0530

    Intended properly

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
 	description='CLI for downloading sub-directory of any Github repository!',
 	url='https://github.com/amarlearning/Github-Sectory',
 	author='Amar Prakash Pandey (@amarlearning)',
-    author_email='amar.om1994@gmail.com',
+	author_email='amar.om1994@gmail.com',
 	license='MIT',
 	keywords='github repository download sub-directory',
 	install_requires=['clint'],
